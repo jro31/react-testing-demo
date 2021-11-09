5b4d30f176d11d32f85512896fd960bd86272b73
Jethro Williams <jethrowilliams@outlook.com>
2021-11-09 11:36:47 +0000

    Add two more test, including one that tests the outcome of a button click

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,17 +1,32 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
 describe('Greeting component', () => {
-  test('renders Hello World as text', () => {
+  test('renders "Hello World" as text', () => {
+    render(<Greeting />);
+
+    const helloWorldElement = screen.getByText('Hello World!');
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test('renders "good to see you" if the button was NOT clicked', () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText('good to see you', { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('renders "Changed!" if the button was clicked', () => {
     // Arrange
     render(<Greeting />);
 
     // Act
-    // ... nothing
+    const buttonElement = screen.getByRole('button'); // Could also get it by the button text using 'getByText'
+    userEvent.click(buttonElement);
 
     // Assert
-    const helloWorldElement = screen.getByText('Hello World!');
-
-    expect(helloWorldElement).toBeInTheDocument();
+    const outputElement = screen.getByText('Changed!');
+    expect(outputElement).toBeInTheDocument();
   });
 });
