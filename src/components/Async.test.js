15c8d024ad1a5b8e607b225913eb4a6b4c2c5032
Jethro Williams <jethrowilliams@outlook.com>
2021-11-09 12:34:47 +0000

    Replace calling the API in the test, by mocking the API return

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,15 +3,13 @@ import Async from './Async';
 
 describe('Async component', () => {
   test('renders posts if request succeeds', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p1', title: 'First post' }],
+    });
     render(<Async />);
 
-    // We use 'findAllByRole()' (instead of 'getAllByRole()') because the <Async> component runs an asynchronous function
-    // 'get' assesses the items that are immediately on the screen
-    // 'find' returns a promise, and React 'testing-library' will re-evaluate the screen mutiple times, until 'screen.findAllByRole('listitem')' succeeds
-    // (we can't just use 'findByRole()' here, because there will be multiple '<li>' elements, so it would fail)
-    // 'findAllByRole' accepts an optional third argument (the second being the '{ exact: true }' object). The third argument sets the time to wait until 'screen.findAllByRole('listitem')' must be true
-    // The default is 1 second (no example was given how to set this time, but I think in an object)
-    const listItemElements = await screen.findAllByRole('listitem'); // See 'https://www.w3.org/TR/html-aria/#docconformance' for roles
+    const listItemElements = await screen.findAllByRole('listitem');
     expect(listItemElements).not.toHaveLength(0);
   });
 });
